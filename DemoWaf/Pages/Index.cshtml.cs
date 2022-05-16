122d346ccc63d24bb2ed7c5f28d87890c2f44c82
Frédo <fmirouze@hotmail.com>
2022-05-16 22:48:12 +0200

    Ajoutez des fichiers projet.

---
+++ DemoWaf/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWaf.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public IndexModel()
+        {
+          
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(string cadeau)
+        {
+            return RedirectToPage("./Merci");
+        }
+    }
+}
---
+++ DemoWaf/Pages/Merci.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWaf.Pages
+{
+    public class MerciModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
